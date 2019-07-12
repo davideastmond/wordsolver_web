bbb9b14bb3cc15ae49e00542b57619b93b089237
davideastmond <davebuck1981@gmail.com>
2019-07-12 06:58:21 -0400

    basic inclusive filter works, UI updates

--- src/App.js
+++ src/App.js
@@ -44,14 +44,21 @@ class App extends Component {
 
 	getInputArray = (e, filter_array) =>  {
 		// This is in charge of getting the array of characters used for a filter. This is passed down through props
-		console.log("Filter array", filter_array);
+		
 
 		// We've now got a filter string and a query type
 		// console.log(this.state.sortedWordList);
 		
 		if (filter_array.queryType === "Basic Filter"){
 			const result = doBasicInclusiveFilter(this.state.sortedWordList, filter_array.queryArray);
-			console.log("result is", result);
+			console.log("result is", result); 
+			
+			this.setState({sortedWordList: result}, ()=> {
+				const wordListBox = document.getElementById('internal-wordlist-textarea');
+				wordListBox.value = this.state.sortedWordList;
+				console.log(this.state.sortedWordList);
+			});
+		
 		}
 	}
 
